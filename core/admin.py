f6dd0123578540dad34426cd238b7590b84ea69c
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-22 19:53:57 +0500

    usercration and chane form

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
