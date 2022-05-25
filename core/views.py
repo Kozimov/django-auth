011b9197cd9f9b2dfadd26790c786aa72694dcea
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-25 14:07:36 +0500

    signup

--- core/views.py
+++ core/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
+
 
 def home(request):
     return render(request, 'index.html')
+
+
+class SignupView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = "/accounts/login"
+    template_name = "registration/signup.html"
--- demo/urls.py
+++ demo/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home
+from core.views import home, SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
+    path('signup', SignupView.as_view(), name="signup"),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
